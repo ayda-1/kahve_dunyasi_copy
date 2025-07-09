628793452af9782e5aa8e452bc99f59a57f73ef0
“ayda-1” <xjuviax@gmail.com>
2025-07-09 15:59:10 +0300

    giriş işlemleri

--- lib/entry_screen.dart
+++ lib/entry_screen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
-class EntryPage extends StatelessWidget {
-  const EntryPage({super.key});
+class EntryScreen extends StatelessWidget {
+  const EntryScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
--- lib/route_generator.dart
+++ lib/route_generator.dart
@@ -1,15 +1,16 @@
+import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:kahve_dunyasi/entry_page.dart';
+import 'package:kahve_dunyasi/entry_screen.dart';
 
 class RouteGenerator {
   static Route<dynamic>? routeGenerator(RouteSettings settings) {
     if (settings.name == "/") {
-    } else if (settings.name == "") {
-    } else if (settings.name == "") {
-    } else if (settings.name == "") {
-    } else if (settings.name == "") {
-    } else if (settings.name == "") {
-    } else if (settings.name == "") {
-    } else if (settings.name == "") {
-    } else if (settings.name == "") {}
+      return MaterialPageRoute(builder: (context) => EntryPage());
+    } else if (settings.name == "/entryScreen") {
+      return MaterialPageRoute(builder: (context) => EntryScreen());
+    } else if (settings.name == "/entryPage") {
+      return MaterialPageRoute(builder: (context) => EntryPage());
+    }
   }
 }
