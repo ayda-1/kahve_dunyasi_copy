5af1aa1c6d1cefcce3b59e2151057dfa9b3ba2f5
ayda-1 <xjuviax@gmail.com>
2025-07-15 18:12:55 +0300

    deneme ekranı silindi

--- lib/route_generator.dart
+++ lib/route_generator.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:kahve_dunyasi/entry_page.dart';
-import 'package:kahve_dunyasi/entry_screen.dart';
 import 'package:kahve_dunyasi/online_shop_page.dart';
 import 'package:kahve_dunyasi/take_away_bottom_navigation_bar.dart';
 import 'package:kahve_dunyasi/take_away_page.dart';
@@ -10,9 +9,7 @@ class RouteGenerator {
   static Route<dynamic>? routeGenerator(RouteSettings settings) {
     if (settings.name == "/") {
       return MaterialPageRoute(builder: (context) => EntryPage());
-    } else if (settings.name == "/entryScreen") {
-      return MaterialPageRoute(builder: (context) => EntryScreen());
-    } else if (settings.name == "/entryPage") {
+    }  else if (settings.name == "/entryPage") {
       return MaterialPageRoute(builder: (context) => EntryPage());
     }else if (settings.name == "/takeAwayPage") {
       return MaterialPageRoute(builder: (context) => TakeAwayBottomNavigationBar());
